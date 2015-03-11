9962a95101f1ab0382e2bbcfa3e35aa80c7d78a5
Hwidong Bae <spilist@gmail.com>
2015-03-11 15:47:47 +0900

    Monthly_goal references User

--- db/migrate/20150311054116_create_monthly_goals.rb
+++ db/migrate/20150311054116_create_monthly_goals.rb
@@ -2,7 +2,8 @@ class CreateMonthlyGoals < ActiveRecord::Migration
   def change
     create_table :monthly_goals do |t|
       t.text :description, null:false, default: ""
-      t.date :season, null:false, default: Time.current.to_date.beginning_of_month
+      t.date :season, null:false, index: true, default: Time.current.to_date.beginning_of_month
+      t.references :user, index: true, null:false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,13 @@ ActiveRecord::Schema.define(version: 20150311054116) do
   create_table "monthly_goals", force: :cascade do |t|
     t.text     "description", default: "",           null: false
     t.date     "season",      default: '2015-03-01', null: false
+    t.integer  "user_id",                            null: false
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
   end
+
+  add_index "monthly_goals", ["season"], name: "index_monthly_goals_on_season", using: :btree
+  add_index "monthly_goals", ["user_id"], name: "index_monthly_goals_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "", null: false
