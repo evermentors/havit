20be9fad54ae341013d345700d5e396627d742fd
Hwidong Bae <spilist@gmail.com>
2015-03-13 17:32:54 +0900

    Add buttons for setting/editing weekly/daily goals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,25 @@
 module ApplicationHelper
-  def no_goal?
+  def no_monthly_goal?
     MonthlyGoal.of(current_user).count.zero?
   end
 
-  def current_goal
+  def current_monthly_goal
     MonthlyGoal.of(current_user).last
+  end
+
+  def no_weekly_goal?
+    WeeklyGoal.of(current_user).count.zero?
+  end
+
+  def current_weekly_goal
+    WeeklyGoal.of(current_user).last
+  end
+
+  def no_daily_goal?
+    DailyGoal.of(current_user).count.zero?
+  end
+
+  def current_daily_goal
+    DailyGoal.of(current_user).last
   end
 end
