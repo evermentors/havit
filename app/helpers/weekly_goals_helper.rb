a8f4ca93c161ad856d60b7c32f49e16d950dac22
Hwidong Bae <spilist@gmail.com>
2015-04-02 22:47:48 +0900

    Add last weekly goal helper

--- app/helpers/weekly_goals_helper.rb
+++ app/helpers/weekly_goals_helper.rb
@@ -7,6 +7,10 @@ module WeeklyGoalsHelper
     WeeklyGoal.of(user, date.beginning_of_week).last
   end
 
+  def last_weekly_goal
+    WeeklyGoal.where(user_id: current_user.id).order(created_at: :desc).first
+  end
+
   # TO BE MODIFIED
   # def relative_weeknum (date=Time.current.to_date)
   #   Time.current.to_date.strftime("%W").to_i - current_season.strftime("%W").to_i + 1
