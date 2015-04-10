36bd272bbd6cb0628b40c6d5b85d9b07c4e96d61
Hwidong Bae <spilist@gmail.com>
2015-04-10 17:59:55 +0900

    Change migration for last_checked_notification

--- db/migrate/20150410025048_add_last_checked_notification_to_user.rb
+++ db/migrate/20150410025048_add_last_checked_notification_to_user.rb
@@ -1,5 +1,11 @@
 class AddLastCheckedNotificationToUser < ActiveRecord::Migration
   def change
-    add_column :users, :last_checked_notification, :integer, default: 1, null: false
+    add_column :users, :last_checked_notification, :integer
+
+    User.find_each do |user|
+      user.update last_checked_notification: Notification.last.id
+    end
+
+    change_column :users, :last_checked_notification, :integer, null: false
   end
 end
