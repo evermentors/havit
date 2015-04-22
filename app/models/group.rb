4714420d722e41d6280f040410d0df68ec34d48d
Hwidong Bae <spilist@gmail.com>
2015-04-22 20:51:29 +0900

    Limit group description as 200 characters

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,7 @@ class Group < ActiveRecord::Base
   has_many :statuses, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
+  validates :description, length: { maximum: 200 }
   validates :password, length: { maximum: 20 }
   validates :member_limit, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :creator, presence: true
