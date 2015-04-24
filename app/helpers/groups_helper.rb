e157e8aad19ec04053e3102b53a2e1190fa1bd4c
Hwidong Bae <spilist@gmail.com>
2015-04-24 17:38:35 +0900

    Add is_full? helper for group search result

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -7,6 +7,10 @@ module GroupsHelper
     return groups
   end
 
+  def is_full? (group)
+    group.member_limit > 0 and group.characters.count >= group.member_limit
+  end
+
   def joined_group? (group, user=current_user)
     not Character.in_group(user, group).blank?
   end
