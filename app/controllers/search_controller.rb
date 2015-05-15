8339a59063f6e31b8aa022053ae5169537ee62e0
Hwidong Bae <spilist@gmail.com>
2015-05-15 10:27:11 +0900

    Minor improvements on search#index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -21,14 +21,18 @@ class SearchController < ApplicationController
 
     @groups_joined = []
     @groups_not_joined = []
+    @groups_not_joined_count = 0
 
     groups.each do |g|
       if view_context.joined_group?(g)
         @groups_joined << g
       else
         @groups_not_joined << g
+        @groups_not_joined_count += 1
       end
     end
+
+    @groups_count = groups.count
   end
 
   private
