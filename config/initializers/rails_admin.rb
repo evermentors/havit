f7c56174696a0edcf94e416d310cd373ee7cb0c7
Hwidong Bae <spilist@gmail.com>
2015-05-20 18:17:30 +0900

    Only evermentors@gmail.com can access to admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -7,6 +7,11 @@ RailsAdmin.config do |config|
   #   warden.authenticate! scope: :user
   # end
   # config.current_user_method(&:current_user)
+  RailsAdmin.config do |config|
+    config.authorize_with do
+      redirect_to main_app.root_path unless warden.user.email == 'evermentors@gmail.com'
+    end
+  end
 
   ## == Cancan ==
   # config.authorize_with :cancan
