7c961d76d0efe12aa5e176c97ebcf1dc77eea64c
Hwidong Bae <spilist@gmail.com>
2015-06-15 16:17:37 +0900

    Temporarily

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -12,7 +12,7 @@ class Goal < ActiveRecord::Base
   validates :description, presence: true
   validates :type, presence: true
   validates :type_specific_fields, presence: true
-  validate :end_date_sholud_be_future
+  # validate :end_date_sholud_be_future
 
   def end_date_sholud_be_future
     errors.add(:end_date, "실천 종료일을 제대로 골라주세요.") if
