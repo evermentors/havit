66518ea58dc467c2f2ec4516fc09b299d205641a
Hwidong Bae <spilist@gmail.com>
2015-06-23 18:17:03 +0900

    Show goal period on hover for progress bar

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -4,4 +4,8 @@ module GoalsHelper
     denominator = (goal.end_date - goal.created_at.to_date).to_i
     (numerator * 100 / denominator).to_f
   end
+
+  def goal_period (goal)
+    goal.created_at.strftime("%-y년 %-m월 %-d일") + ' ~ ' + goal.end_date.strftime("%-y년 %-m월 %-d일")
+  end
 end
